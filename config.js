1d4eb1decf71a615efe06bab248661fd84ba8efa
theanh <theanh@example.com>
2022-03-14 19:01:40 +0700

    gg

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '🐷';
+const giftUrl = 'https://theanh123789.github.io/8-3/';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Tặng Bạn nè';
+const contentCard = '8/3 tập làm phụ nữ đi nhóe :v';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'ann-e.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
